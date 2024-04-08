d4964c7c632e209126c24ea49401f9fa6558ee88
Daniela Alves <dani.alvesm22@gmail.com>
2024-04-08 13:47:33 -0300

    :wrench: criando services firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBmT0lcd3_pTVIwLFvz07dKyTmRbSYBxUY",
+  authDomain: "teste-alvesxdani.firebaseapp.com",
+  projectId: "teste-alvesxdani",
+  storageBucket: "teste-alvesxdani.appspot.com",
+  messagingSenderId: "656829799237",
+  appId: "1:656829799237:web:0ac40e3e2fbd13bedbfc2c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
