e8f06c197e90856a7b81b270961199132a94de53
Daniela Alves <dani.alvesm22@gmail.com>
2024-04-09 13:28:27 -0300

    :spakles: criação da pasta interfaces

---
+++ src/Interfaces/index.ts
@@ -0,0 +1,7 @@
+// Define a user type that includes the Firebase user object
+export interface IUser {
+    id: string;
+    email: string;
+    displayName: string | null;
+    photoURL: string | null;
+  };
