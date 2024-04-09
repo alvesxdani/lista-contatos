69af353583715bbb161510d8d0822a29bfbf366a
alvesxdani <dani.alvesm22@gmail.com>
2024-04-08 23:40:37 -0300

    :sparkles: inserção do componente de input

--- src/containers/FormsLogin/index.tsx
+++ src/containers/FormsLogin/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { StyledFormsLogin } from './styles'
 import Subtitulo from '../../components/Subtitulo'
+import Input from '../../components/Input'
 
 const FormsLogin = () => {
   return (
     <StyledFormsLogin>
         <Subtitulo text='Faça login'/>
+        <Input type='email' label='E-mail' id='email' name='email' />
     </StyledFormsLogin>
   )
 }
