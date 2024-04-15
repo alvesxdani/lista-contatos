9bdc71a836e4314821316ce5da2b2e2bf97278a4
alvesxdani <dani.alvesm22@gmail.com>
2024-04-15 01:34:43 -0300

    :bricks: ci: criando const para auth e db do firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,17 +1,24 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBmT0lcd3_pTVIwLFvz07dKyTmRbSYBxUY",
-  authDomain: "teste-alvesxdani.firebaseapp.com",
-  projectId: "teste-alvesxdani",
-  storageBucket: "teste-alvesxdani.appspot.com",
-  messagingSenderId: "656829799237",
-  appId: "1:656829799237:web:0ac40e3e2fbd13bedbfc2c"
-};
+  apiKey: 'AIzaSyBmT0lcd3_pTVIwLFvz07dKyTmRbSYBxUY',
+  authDomain: 'teste-alvesxdani.firebaseapp.com',
+  projectId: 'teste-alvesxdani',
+  storageBucket: 'teste-alvesxdani.appspot.com',
+  messagingSenderId: '656829799237',
+  appId: '1:656829799237:web:0ac40e3e2fbd13bedbfc2c',
+}
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig)
+
+const db = getFirestore(app)
+const auth = getAuth(app)
+
+export { db, auth }
