4aaddacf5271edd0d21fd457b1ce57d79dc7acc2
alvesxdani <dani.alvesm22@gmail.com>
2024-04-14 23:06:04 -0300

    :lipstick: estilizando o componente input

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -4,13 +4,16 @@ import colors from "../../styles/colors";
 export const StyledInput = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: start;
   gap: 1rem;
   width: 100%;
+  font-weight: bold;
+  margin-bottom: 1.2rem;
   input {
     padding: 0.7rem;
     border-radius: 0.5rem;
     border: 1px solid ${colors.grey};
+    background: #f7f5f5;
     width: 100%;
   }
-  `
+`
