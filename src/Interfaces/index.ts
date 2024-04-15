2a3ff3a64fda63fa21148eebfabcaef9612d46ac
alvesxdani <dani.alvesm22@gmail.com>
2024-04-15 01:07:53 -0300

    :sparkles: criando interface para o resolver do form

--- src/Interfaces/index.ts
+++ src/Interfaces/index.ts
@@ -1,7 +1,12 @@
 // Define a user type that includes the Firebase user object
 export interface IUser {
-    id: string;
-    email: string;
-    displayName: string | null;
-    photoURL: string | null;
-  };
+  id: string
+  email: string
+  displayName: string | null
+  photoURL: string | null
+}
+
+export interface IAuthForm {
+  email: string
+  password: string
+}
