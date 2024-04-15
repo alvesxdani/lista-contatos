45ec87bd5e3fa2c94436c23e2ad3b061464faf09
alvesxdani <dani.alvesm22@gmail.com>
2024-04-14 23:05:47 -0300

    :recycle: refactor: refatorando o componente input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,18 +1,15 @@
 import React, { InputHTMLAttributes } from 'react'
 import { StyledInput } from './styles'
 
-type InputProps = InputHTMLAttributes<{
-  type: string
-  value: string
-  placeholder?: string
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
-  onClick?: (e: React.MouseEvent) => void
-}>
+type InputProps = InputHTMLAttributes<HTMLButtonElement> & {
+  label?: string
+}
 
-const Input = ({ name, id, type, placeholder }: InputProps) => {
+const Input = ({ id, type, placeholder, label }: InputProps) => {
   return (
     <StyledInput>
-      <input type={type} id={id} name={name} placeholder={placeholder} />
+      <label htmlFor={id}>{label}</label>
+      <input type={type} id={id} name={id} placeholder={placeholder} />
     </StyledInput>
   )
 }
