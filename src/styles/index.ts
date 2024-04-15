967cf77dbccb71de0a5d5ccbe9acc52be73e289e
alvesxdani <dani.alvesm22@gmail.com>
2024-04-14 23:05:18 -0300

    :lipstick: estilização do componente global de botão

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,5 +1,10 @@
-import { createGlobalStyle } from "styled-components";
-import colors from "./colors";
+import styled, { createGlobalStyle } from 'styled-components'
+import colors from './colors'
+import { ButtonHTMLAttributes } from 'react'
+
+export type TButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  color: 'red' | 'black' | 'grey'
+}
 
 export const GlobalStyle = createGlobalStyle`
   * {
@@ -12,4 +17,21 @@ export const GlobalStyle = createGlobalStyle`
   body {
     color: ${colors.black};
   }
+`
+
+export const StyledButton = styled.button<TButtonProps>`
+  background: ${(props) =>
+    props.color === 'red'
+      ? colors.red
+      : props.color === 'black'
+      ? colors.black
+      : props.color === 'grey'
+      ? colors.grey
+      : '#fff'};
+      color: #fff;
+      padding: 12px 10px;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+      font-weight: bold;
 `
