32259d128e61ea4a2c93c060cbb6382122be9b83
alvesxdani <dani.alvesm22@gmail.com>
2024-04-15 01:32:49 -0300

    :recycle: refactor: refatorando o componente

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,17 +1,26 @@
-import React, { InputHTMLAttributes } from 'react'
+import React, { InputHTMLAttributes, forwardRef } from 'react'
 import { StyledInput } from './styles'
 
 type InputProps = InputHTMLAttributes<HTMLInputElement> & {
   label?: string
 }
 
-const Input = ({ id, type, placeholder, label, name, ...props }: InputProps) => {
-  return (
-    <StyledInput>
-      <label htmlFor={id}>{label}</label>
-      <input type={type} id={id} name={name} placeholder={placeholder} {...props} />
-    </StyledInput>
-  )
-}
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ id, type, placeholder, label, name, ...props }, ref) => {
+    return (
+      <StyledInput>
+        <label htmlFor={id}>{label}</label>
+        <input
+          type={type}
+          id={id}
+          name={name}
+          placeholder={placeholder}
+          ref={ref}
+          {...props}
+        />
+      </StyledInput>
+    )
+  },
+)
 
 export default Input
