c6d69ff749838a17eadd19f50b0224ed00a75ed9
alvesxdani <dani.alvesm22@gmail.com>
2024-04-20 15:20:19 -0300

    :sparkles: criando useAuth hook

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,14 @@
+import { checkLoginStatus } from '../store/reducers/login'
+import { useAppDispatch, useAppSelector } from './storeHook'
+
+export const useAuth = () => {
+  const { user } = useAppSelector((state) => state.auth)
+  const dispatch = useAppDispatch()
+
+  function getAuth() {
+    const authStatus = dispatch(checkLoginStatus())
+    return authStatus
+  }
+
+  return { user, getAuth }
+}
