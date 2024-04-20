341a5b2f60a55b0ef6d5a27a9ef26b60f4228a7d
alvesxdani <dani.alvesm22@gmail.com>
2024-04-20 15:52:23 -0300

    :sparkles: criando a header da página

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Logo from '../Logo'
+import { useAuth } from '../../hooks/useAuth'
+import { StyledHeader } from './styles';
+import Button from '../Botao';
+import { useAppDispatch } from '../../hooks/storeHook';
+import { logout } from '../../store/reducers/login';
+import { useNavigate } from 'react-router-dom';
+
+const Header = () => {
+  const {user} = useAuth();
+  const dispatch = useAppDispatch()
+  const navigate = useNavigate()
+
+
+  function handleLogout() {
+    dispatch(logout())
+    navigate('/')
+  }
+  return (
+    <StyledHeader>
+      <Logo />
+      <div className="info--user">
+        <img
+          src={
+            user?.photoURL ||
+            'https://th.bing.com/th/id/OIP.4yQU3p-Cx4P_mLF7QcesFwAAAA?rs=1&pid=ImgDetMain'
+          }
+          alt=""
+        />
+        {user?.email}
+        <Button color="red" onClick={handleLogout}>
+          Logout
+        </Button>
+      </div>
+    </StyledHeader>
+  )
+}
+
+export default Header
