88ac46a74dfdffb8382ee48980e909407a0e78b4
alvesxdani <dani.alvesm22@gmail.com>
2024-04-20 15:23:33 -0300

    :construction: construindo a página home de contatos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
-import { useAppSelector } from '../../hooks/storeHook'
+import React, { useEffect, useState } from 'react'
+import { useAppDispatch, useAppSelector } from '../../hooks/storeHook'
+import { checkLoginStatus, logout } from '../../store/reducers/login'
+import { useNavigate } from 'react-router-dom'
+import Button from '../../components/Botao'
+import { useAuth } from '../../hooks/useAuth'
 
 const Home = () => {
-  const {user} = useAppSelector((state) => state.auth)
-  return (
-    <p>Bem-vindo, {user?.email}</p>
-  )
+  // const { user } = useAppSelector((state) => state.auth)
+  const {user, getAuth} = useAuth()
+  const dispatch = useAppDispatch()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    console.log(user)
+    getAuth()
+    if(!user) navigate('/')
+  }, [])
+
+  function handleLogout() {
+    dispatch(logout())
+    navigate('/')
+  }
+
+  if (user)
+    return (
+      <>
+        <p>Bem-vindo, {user?.email}</p>
+        <Button color='black' onClick={handleLogout}>Logout</Button>
+      </>
+    )
 }
 
 export default Home
