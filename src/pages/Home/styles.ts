3fb99c9ae7f1c0728790868cd5bae561f5a56d66
alvesxdani <dani.alvesm22@gmail.com>
2024-04-20 20:24:38 -0300

    :lipstick: estilizando o container

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledContainerHome = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 0.7fr 3fr;
+  justify-items: center;
+`
