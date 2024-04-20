6da1461ef6469a5b744a6e7ea6595f946207e868
alvesxdani <dani.alvesm22@gmail.com>
2024-04-20 20:21:19 -0300

    :recycle: refactor: reajustando as rotas

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,28 @@ import { Provider } from 'react-redux'
 import store from './store'
 import Login from './pages/Login'
 import Home from './pages/Home'
+import ListaContatos from './containers/ListaContatos'
+import NovoContato from './containers/NovoContato'
 
 function App() {
   const router = createBrowserRouter([
     {
-      path: '/',
+      path: '/login',
       element: <Login />,
     },
     {
-      path: '/index',
+      path: '/',
       element: <Home />,
+      children: [
+        {
+          path: '/',
+          element: <ListaContatos />,
+        },
+        {
+          path: '/novo-contato',
+          element: <NovoContato />,
+        }
+      ]
     },
   ])
   return (
