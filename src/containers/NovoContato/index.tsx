e52ffb2fac49523a6c50da78dc37a139f61da63f
alvesxdani <dani.alvesm22@gmail.com>
2024-04-20 20:22:48 -0300

    :construction: construindo o container do form de novos contatos

---
+++ src/containers/NovoContato/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { StyledNovoContato } from './styles'
+import Subtitulo from '../../components/Subtitulo'
+import Input from '../../components/Input'
+import Button from '../../components/Botao'
+
+const NovoContato = () => {
+  return (
+    <StyledNovoContato>
+      <Subtitulo text="Adicione um novo contato" />
+      <Input type="text" label="Nome" id="name-contact" name="name-contact" />
+      <Input type="tel" label="Telefone" id="tel-contact" name="tel-contact" />
+      <Button color='black'>Salvar</Button>
+    </StyledNovoContato>
+  )
+}
+
+export default NovoContato
