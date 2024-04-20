3dc26cba5db6941bcce1d8c7125c6b406e4e8ba0
alvesxdani <dani.alvesm22@gmail.com>
2024-04-20 17:12:39 -0300

    :broom: cleanup: linha desnecessária

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Logo from '../../components/Logo'
 import { StyledContainer } from './styles'
 import FormsLogin from '../../containers/FormsLogin'
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from '../../hooks/useAuth'
 
 const Login = () => {
-  return (
-    <StyledContainer>
-      <Logo />
-      <FormsLogin />
-    </StyledContainer>
-  )
+  const { user, getAuth } = useAuth()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    getAuth()
+    if (user) navigate('/index')
+  }, [])
+
+    return (
+      <StyledContainer>
+        <Logo />
+        <FormsLogin />
+      </StyledContainer>
+    )
 }
 
 export default Login
