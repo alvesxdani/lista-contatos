3bc16ac19cde2631c943d5cd4a1f16f4d1b3560f
alvesxdani <dani.alvesm22@gmail.com>
2024-04-20 23:16:25 -0300

    :broom: removendo linhas desnecessárias

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,5 +1,5 @@
-import styled from "styled-components";
-import colors from "../../styles/colors";
+import styled from 'styled-components'
+import colors from '../../styles/colors'
 
 export const StyledInput = styled.div`
   display: flex;
