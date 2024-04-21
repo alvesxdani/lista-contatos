46b09411693ded4f41f666ab3ebd64b035c385f5
alvesxdani <dani.alvesm22@gmail.com>
2024-04-20 23:17:12 -0300

    :recycle: refactor: schema para forms de novos contatos

--- src/models/FormNovoContato.ts
+++ src/models/FormNovoContato.ts
@@ -1,5 +1,7 @@
 import * as yup from 'yup'
 
+const phoneRegex = /^\+?\d{2}\s?\(?\d{2}\)?\s?\d{4,5}-?\d{4}$/
+
 export const newContactSchema = yup.object().shape({
   name: yup.string().uppercase().trim().required('O nome é obrigatório'),
   email: yup
@@ -8,5 +10,6 @@ export const newContactSchema = yup.object().shape({
     .required('E-mail obrigatório'),
   phone: yup
     .string()
-    .required('O telefone é obrigatório')
+    .matches(/^\(\d{2}\) \d{5}-\d{4}$/, 'Formato de telefone inválido')
+    .required('Telefone é obrigatório'),
 })
