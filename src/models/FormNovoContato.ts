c438c94844e1e145e91cd037ed64740608eb354f
alvesxdani <dani.alvesm22@gmail.com>
2024-04-20 21:23:36 -0300

    :sparkles: criando schema para novo contato

--- src/models/FormNovoContato.ts
+++ src/models/FormNovoContato.ts
@@ -1,9 +1,12 @@
 import * as yup from 'yup'
 
-export const novoContatoSchema = yup.object().shape({
-  name: yup.string().uppercase().trim().required(),
+export const newContactSchema = yup.object().shape({
+  name: yup.string().uppercase().trim().required('O nome é obrigatório'),
   email: yup
     .string()
     .email('Digite um e-mail válido.')
     .required('E-mail obrigatório'),
+  phone: yup
+    .string()
+    .required('O telefone é obrigatório')
 })
