665b763122d9a13404d28c9b9bdceb3ad5b5cb74
alvesxdani <dani.alvesm22@gmail.com>
2024-04-22 16:22:31 -0300

    :lipstick: estilizando o forms do componente

---
+++ src/containers/FormContato/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledNovoContato = styled.form`
+  width: 60%;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`
