9c7f7741c9f34bd7ddb2dd0f78f5cfcb6a84d253
alvesxdani <dani.alvesm22@gmail.com>
2024-04-22 16:24:01 -0300

    :sparkles: feat: lógica botão para remover filtrado pelo id do contato

--- src/containers/ListaContatos/index.tsx
+++ src/containers/ListaContatos/index.tsx
@@ -1,16 +1,15 @@
-import React, { MouseEventHandler, SetStateAction, useEffect, useState } from 'react'
-import { DocumentData, collection, getDocs } from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
+import { deleteDoc, doc, getDocs } from 'firebase/firestore'
 import { StyledListaContatos } from './styles'
 import Subtitulo from '../../components/Subtitulo'
 import { db } from '../../services/firebase'
 import Button from '../../components/Botao'
 import { useNavigate } from 'react-router-dom'
 import { Container } from '../../styles'
-import { useAppDispatch, useAppSelector } from '../../hooks/useApp'
-import { fetchItens } from '../../store/reducers/contatos'
 import { useFetchItens } from '../../hooks/useFetchItens'
 
 const ListaContatos = () => {
+  const [isDeleting, setIsDeleting] = useState<string | null>()
   const navigate = useNavigate()
   const { items, fetchContatos } = useFetchItens()
 
@@ -22,6 +21,20 @@ const ListaContatos = () => {
     // Lógica para levar ao id dos dados
     navigate(`/editar-contato/${id}`)
   }
+
+  async function handleDelete(id: string) {
+    try {
+      setIsDeleting(id)
+      await deleteDoc(doc(db, 'users', `${id}`))
+      fetchContatos()
+    } catch (error) {
+      console.error('Error deleting document', error)
+      throw error
+    } finally {
+      setIsDeleting(null)
+    }
+  }
+
   return (
     <Container>
       <Subtitulo text="Lista de contatos" />
@@ -32,7 +45,14 @@ const ListaContatos = () => {
               <h2>{data.name}</h2>
               <p>{data.phone}</p>
               <p>{data.email}</p>
-              <Button color="red" onClick={() => handleEdit(id)}>Editar</Button>
+              <div>
+                <Button color="black" onClick={() => handleEdit(id)}>
+                  Editar
+                </Button>
+                <Button color="red" onClick={() => handleDelete(id)}>
+                  {isDeleting === id ? 'Excluindo...' : 'Excluir'}
+                </Button>
+              </div>
             </li>
           )
         })}
