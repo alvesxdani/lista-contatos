5dc3b071a99792ec2487bb385336a296ed01dd6e
alvesxdani <dani.alvesm22@gmail.com>
2024-04-21 22:30:54 -0300

    :lipstick: feat: estilizando o componente

--- src/containers/ListaContatos/styles.ts
+++ src/containers/ListaContatos/styles.ts
@@ -1,20 +1,17 @@
-import styled from "styled-components";
-import colors from "../../styles/colors";
+import styled from 'styled-components'
+import colors from '../../styles/colors'
 
-export const StyledListaContatos = styled.div`
-  width: 100%;
+export const StyledListaContatos = styled.ul`
   padding: 1rem;
-  ul {
+  list-style: none;
+  li {
+    display: flex;
+    align-items: start;
+    flex-direction: column;
+    gap: 1rem;
+    background-color: ${colors.grey};
     padding: 1rem;
-    list-style: none;
-    li {
-      display: flex;
-      flex-direction: column;
-      gap: 1rem;
-      background-color: ${colors.grey};
-      padding: 1rem;
-      margin-bottom: 2rem;
-      border-radius: 0.5rem;
-    }
+    margin-bottom: 2rem;
+    border-radius: 0.5rem;
   }
 `
