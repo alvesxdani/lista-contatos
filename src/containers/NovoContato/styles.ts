a7a04b07ac4db6569d9ebd97193004fbe87dd206
alvesxdani <dani.alvesm22@gmail.com>
2024-04-22 16:06:04 -0300

    :wastebasket: remove: arquivos desnecessários

--- src/containers/NovoContato/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const StyledNovoContato = styled.form`
-  width: 60%;
-  padding: 2rem;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-`
