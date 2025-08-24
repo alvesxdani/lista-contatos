9c08dc7194df6ca8afdd698175cf51af27d8cc53
Daniela Alves <dani.alvesm22@gmail.com>
2025-08-24 18:06:54 -0300

    chore: update TypeScript configuration and enhance Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`,
   },
 })
