8d2a1142f12bbc28bf72f4269f3920fc98ebba9b
Daniela Alves <dani.alvesm22@gmail.com>
2026-04-04 00:25:56 -0300

    feat: adicionando chamada ao database

--- app/api/services/firebase.ts
+++ app/api/services/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
+export const db = getFirestore(app)
